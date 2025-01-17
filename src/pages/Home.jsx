92abcc2e4faf44c4f2a8f534ee041e24d281229c
KaigaMunene <kaigamunene@gmail.com>
2025-01-17 08:51:13 +0300

    feat: create work with us page * create page content * style the page * add images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Contact from './Contact';
 import Hero from './Hero';
 import About from './About';
+import WorkWithUs from './WorkWithUs';
 
 function Home() {
   return (
     <div>
       <Hero />
       <About />
+      <WorkWithUs />
       <Contact />
     </div>
   );
