57d088c51ccfa4bafb8c7d3feb19440247f7f83f
KaigaMunene <kaigamunene@gmail.com>
2025-01-17 16:40:23 +0300

    feat: create testimonial section * create section * add styling * create a error boundary component * install react slick to use slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import Contact from './Contact';
 import Hero from './Hero';
 import About from './About';
 import WorkWithUs from './WorkWithUs';
 import OwnYourMic from './OwnYourMic';
 import WhatIOffer from './WhatIOffer';
 import Conversations from './Conversations';
+import TestimonialSlider from './Testimonial';
+import Contact from './Contact';
 
 function Home() {
   return (
@@ -15,6 +16,7 @@ function Home() {
       <WorkWithUs />
       <WhatIOffer />
       <Conversations />
+      <TestimonialSlider />
       <Contact />
     </div>
   );
